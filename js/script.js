9ed6076442383e2a65aafc00618ecb4217de990b
Anton <antoniom@mail.ru>
2022-04-21 20:01:01 +0300

    20.04-карточки создаем через Классы- урок47-49

--- js/script.js
+++ js/script.js
@@ -120,23 +120,21 @@ function setClock(selector, endtime) {
 setClock('.timer', deadLine ); // deadline далее в параметрах как endtime везде
 
 
-// модальные окна
+// +++++++++++++++++++++++++++++++ модальные окна
 const modal = document.querySelector('.modal');
 const btnOpenModal = document.querySelectorAll('[data-modal]');
 const btnCloseModal = document.querySelector('[data-close]');
 
-
+// Суть - автопоказ по скролу и по таймеру всего один раз при загрузке страницы. Если до этого автопоказа юзер сам вызвал модалку то автопоказ убирается навсегда. И потом юзер может только вручную вызывать модалку
 //ф. открытиф модалки
 function openModal() { 
   let offset =  window.outerWidth - window.innerWidth; // 16 px
   modal.style.display = 'block';
   document.body.style.overflow = 'hidden';
   document.body.style.paddingRight = offset + 'px';
-  // убираем показ если юзер сам кликнул и очищаем показ с задержкой
+  // убираем автопоказ если юзер сам кликнул и очищаем показ с задержкой и по скролу
   window.removeEventListener('scroll', showModalByScroll);
   clearTimeout(showModalDelay);
-
-   
 }
 
 // ф. закрытия модалки
@@ -190,7 +188,7 @@ btnOpenModal.forEach(item => {
 
   // Появление модалки при скроле до низа ( минус 300px) или после 10 секунд с начала входа на сайт
   // Для этого создадим функциии открытия модалки и закрытия
-  let showModalDelay = setTimeout( openModal, 5000);
+  // let showModalDelay = setTimeout( openModal, 5000);
 
 // ф. показа при скроле вниз
 function showModalByScroll() { 
@@ -212,12 +210,87 @@ console.log(window.pageYOffset);
 
 console.log(document.documentElement.clientHeight);
 
+// Классы для карточек урок 48
+// Что нужно для карточки - путь к фото, алт.текст для фото, заголовок, описание, цена в долларах
+// Создаем те свойства которые будут меняться с сервера. Размеры будут те же. Класс будет создавать только верстку
+class MenuCard {
+  constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+    this.src = src;
+    this.alt = alt;
+    this.title = title;
+    this.descr = descr;
+    this.price = price;
+    this.classes = classes;
+    this.parent = document.querySelector(parentSelector);
+    this.transfer = 27; // пример курса рубля
+    this.changeToRouble(); // вызываем метод прямо в констр.
+  }
+  changeToRouble() { // метод перевода из дол. в рубли
+this.price = this.price * this.transfer;
+  }
+  render() { // создает верстку в виде элементов
+    const element = document.createElement('div');
+    // если доп. классы не передадутся мы сами их добавим
+    if(this.classes.length ==0) {element.classList.add('menu__item');} 
+   
+   
+    // добавляем класс селектор через оператор rest ( classes) - их может быть несколько  - поэтому через цикл
+    this.classes.forEach(itemClass => { 
+      element.classList.add(itemClass); 
+     });
+    // внутрь пустого div вставляем образец верстки из HTML
+    element.innerHTML = `
+  
+    <img src=${this.src} alt=${this.alt}>
+    <h3 class="menu__item-subtitle">${this.title}</h3>
+    <div class="menu__item-descr">${this.descr}</div>
+    <div class="menu__item-divider"></div>
+    <div class="menu__item-price">
+        <div class="menu__item-cost">Цена:</div>
+        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+    </div>
+
+    `;
+    this.parent.append(element); // вставляем элемент внутрь родителя в конец
+
+  }
+}
+
+// Шаблон класса готов - создаем новые объекты
+// 1 вариант
+let div = new MenuCard( // передаем аргументы через запятую
+"img/tabs/vegy.jpg",
+"vegy",
+'Меню "Фитнес"',
+'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+12,
+'.menu .container',
 
 
+);
+div.render();
+console.log(div);
+// Создаем еще два объекта и добавляем в верстку
+// 2 вар вызова более короткий
+new MenuCard(
+  "img/tabs/elite.jpg",
+"йоги",
+'Меню "Йога"',
+'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+7,
+'.menu .container',
 
+).render();
 
-
-
+new MenuCard(
+  "img/tabs/post.jpg",
+"йоги",
+'Меню "Антон"',
+'Йееей! В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+17,
+'.menu .container',
+'menu__item'
+).render();
 
 
 
